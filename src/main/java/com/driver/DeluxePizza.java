82d1e667d073ebdcbd0ab79b61a93fade4612065
sunidh1908 <sunidh.kumar@gmail.com>
2022-11-28 16:01:45 +0530

    second

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,7 +4,6 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
         addExtraCheese();
         addExtraToppings();
     }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,81 +2,91 @@ package com.driver;
 
 public class Pizza {
 
+    //private final int extraCheesePrice;
     private int price;
     private Boolean isVeg;
     private String bill;
-    private int extraCheesePrice;
+    private int  extraCheesePrice;
+
     private int extraToppingPrice;
+
     private int takeAwayPrice;
 
-    private boolean extraCheese;
-    private boolean extraTopping;
-    private boolean isTakeAway;
+    private boolean isCheeseAdded;
+    private boolean isToppingAdded;
+    private boolean isTakeAwayAdded;
+
     private boolean isBillGenerated;
+
 
     public Pizza(Boolean isVeg){
 
+        this.isCheeseAdded = false;
+        this.isToppingAdded = false;
+        this.isTakeAwayAdded = false;
         this.extraCheesePrice = 80;
-        this.takeAwayPrice = 20;
-        this.extraCheese = false;
-        this.extraTopping = false;
-        this.isTakeAway = false;
         this.isVeg = isVeg;
 
-        if(isVeg == true){
+        this.takeAwayPrice = 20;
+        if(isVeg==true){
+
             this.price = 300;
-            this.extraToppingPrice = 80;
+            this.extraToppingPrice = 70;
         }
-        else {
+        else{
             this.price = 400;
             this.extraToppingPrice = 120;
         }
-        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
+
+        this.bill = "Base Price Of The Pizza: "+this.price + "\n";
+
     }
 
     public int getPrice(){
-
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
-        if(extraCheese == false){
+
+        if(isCheeseAdded==false){
+
             this.price = this.price + this.extraCheesePrice;
-            extraCheese = true;
+            isCheeseAdded = true; //I dont want it to be added repeatedly
         }
     }
 
     public void addExtraToppings(){
-        // your code goes here
-        if(isTakeAway == false){
-            this.price = this.price + this.takeAwayPrice;
-            isTakeAway = true;
+
+        if(isToppingAdded==false){
+            this.price = this.price + this.extraToppingPrice;
+            isToppingAdded = true;
         }
     }
 
     public void addTakeaway(){
-        // your code goes here
-        if(isTakeAway == false){
+
+        if(isTakeAwayAdded==false){
             this.price = this.price + this.takeAwayPrice;
-            isTakeAway = true;
+            isTakeAwayAdded = true;
         }
     }
 
     public String getBill(){
-        // your code goes here
-        if(isBillGenerated == false){
+
+        if(isBillGenerated==false) {
+
             isBillGenerated = true;
-            if(extraCheese == true){
-                this.bill = this.price + "Extra Cheese Added: " + this.extraCheesePrice + "\n";
+
+            if (isCheeseAdded == true) {
+                this.bill = this.bill + "Extra Cheese Added: " + this.extraCheesePrice + "\n";
             }
-            if(extraTopping == true){
-                this.bill = this.price + "Extra Toppings Added: " + this.extraToppingPrice + "\n";
+            if (isToppingAdded == true) {
+                this.bill = this.bill + "Extra Toppings Added: " + this.extraToppingPrice + "\n";
             }
-            if(isTakeAway == true){
-                this.bill = this.price + "Paperbag Added: " + this.takeAwayPrice + "\n";
+            if (isTakeAwayAdded == true) {
+                this.bill = this.bill + "Paperbag Added: " + this.takeAwayPrice + "\n";
             }
-            this.bill = this.bill + "Total Price: " + this.price;
+            this.bill = this.bill + "Total Price: "+this.price + "\n";
         }
         return this.bill;
     }
